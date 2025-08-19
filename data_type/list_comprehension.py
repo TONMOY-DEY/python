a55db508eb6c3e53bd52661e2c721c2b39a3a062
Your Name <your-email@example.com>
2025-08-20 02:34:52 +0600

    one file

---
+++ data_type/list_comprehension.py
@@ -0,0 +1,26 @@
+a=[1,10,23,24,26,90]
+
+result=[]
+
+for i in a:
+    if  i%2==0:
+        result.append(i)
+        print(result)
+
+print('.................................')
+#using list comprehension
+
+new_result=[ i for i in a if i%2==0]
+print(new_result)
+
+
+b=[1,2,3,4,5]
+result=[ i**2 if i%2==0 else i for i in b]
+print(result)
+
+
+
+
+ 
+        
+
---
+++ data_type/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+result1=[ i for i in b if i%2==0]
+# print(result1)
