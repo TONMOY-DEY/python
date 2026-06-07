6d7b2b4c12970be58e478f6f1944f4c5d1225070
TONMOY-DEY <tonmoydey2025@gmail.com>
2026-06-08 00:53:37 +0600

    model training

--- Datathon_Practice_NSU/Main.py
+++ Datathon_Practice_NSU/Main.py
@@ -7,3 +7,24 @@ csv_file = os.path.join(current_dir, "student.csv")
 df = pd.read_csv(csv_file)
 
 print(df)
+
+print(".........................................")
+
+X = df[["Hours_Study","Attendance"]]
+
+y = df["Pass"]
+
+print(X)
+
+print("................................................")
+
+print(y)
+
+from sklearn.tree import DecisionTreeClassifier
+
+model = DecisionTreeClassifier()
+model.fit(X,y)
+
+result = model.predict([[7,90]])
+
+print(result)
